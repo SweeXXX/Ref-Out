b7d8e23758280aab2fb9e1516f002101dfaf4c7c
Nikita Poletaev <97903488+SweeXXX@users.noreply.github.com>
2022-11-16 17:37:54 +0300

    Add files via upload

---
+++ Helpa/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
+using System.Collections;
+
+namespace Home
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            double[] dd = { 1.2, 23.5, 12, 123, 12 };
+            double[] kk = { 1, 2, 3, 4 };
+            K2(dd, kk);
+
+            static void K2(double[] d, double[] k)
+            {
+                //d[0]++;
+                Console.WriteLine(String.Join(" ", d));
+                d = k;
+                d[0]++;
+                k[1]++;
+                Console.WriteLine(string.Join(" ", d));
+                Console.WriteLine(string.Join(" ", k));
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Join(" ", dd));
+            Console.WriteLine(string.Join(" ", kk));
+
+
+            //static void K3(ref string str, out string str2)
+            //{
+            //    str += "O!";
+            //    str2 = str+"МАЙНКРАФТ ЭТО МОЯ ЖИЗНЬ!";
+            //}
+            //string s = "asfa";
+            //string s2 = null;
+            //Console.WriteLine();
+            //Console.WriteLine(s);
+            //Console.WriteLine(s2);
+            //K3(ref s,  out s2);
+            //Console.WriteLine(s);
+            //Console.WriteLine(s2);
+
+
+        }
+
+
+    }
+    
+}
